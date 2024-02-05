225b01da405196db8dce354ba0d84341eaf5cde0
kotinash <92477814+kotinash@users.noreply.github.com>
2024-02-05 22:51:13 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,18 +4,20 @@ type Callback = (...args: any[]) => void
 
 class Event {
 	public name: string
+
 	public fields: any
+
 	public cancelled: boolean = false
+
 	public callback: Callback
 
-	constructor(_name: string, _fields: any, _callback: Callback) {
+	constructor(_name: string, _fields: any, _callback?: Callback) {
 		this.name = _name
 		this.fields = _fields
 		this.callback = _callback
 	}
 
 	public emit(event_emitter: NodeEventEmitter, cancellable: boolean) {
-		// TODO: A better way of doing this?
 		setTimeout(() => {
 			event_emitter.emit(this.name, {
 				...this.fields,
@@ -24,6 +26,7 @@ class Event {
 					if (!cancellable) {
 						throw new Error("Event is not cancellable")
 					}
+
 					this.cancelled = true
 				},
 			})
